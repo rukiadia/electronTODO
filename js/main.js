9d226b512d0fa840c684120ffceb596c8263601d
Sho Yamada <yamadaPC@ymd.local>
2017-03-20 15:53:45 +0900

    処理の構造を変更。オブジェクトリテラル方針にしてみる。

--- js/main.js
+++ js/main.js
@@ -4,96 +4,90 @@ const module = {
   db: null,
   renderer: function(todo){
     var li = document.createElement('li');
-    li.innerHTML = todo.text + ':' + todo.timeStamp;
+    li.innerHTML = todo.text;
     document.querySelector('.todoList').appendChild(li);
-  }
-};
+  },
+  init: function(){
+    // 初期処理
+    var request = window.indexedDB.open('my_db', 3);
 
-module.init = () => {
-  // 以下は初期処理
-  var request = window.indexedDB.open('my_db', 3);
+    request.onupgradeneeded = (event) => {
+      // DBVersionが上がった場合だけ呼ばれる
+      let db = event.target.result;
+      event.target.transaction.onerror = (error) => {
+        alert(error);
+      };
 
-  // DBVersionが上がった場合だけ呼ばれる
-  request.onupgradeneeded = (event) => {
-    let db = event.target.result;
-    event.target.transaction.onerror = (error) => {
-      alert(error);
+      // 既存schema削除
+      if (db.objectStoreNames.contains('todo')) {
+        db.deleteObjectStore('todo');
+      }
+      // 新規作成
+      var store = db.createObjectStore('todo', {
+        // timeStampは一意なので、keyPathとして使う
+        keyPath: 'timeStamp'
+      });
+      console.log('store', store);
     };
 
-    // 既存schema削除
-    if (db.objectStoreNames.contains('todo')) {
-      db.deleteObjectStore('todo');
+    // DBを開くリクエスト。ここが正常に上手くいった場合は、保持
+    request.onsuccess = (event) => {
+      module.db = event.target.result;
+      // 初期表示時に、保存されている物を全て取っておく
+      module.getAllTodo(module.renderer);
     }
-    // 新規作成
-    var store = db.createObjectStore('todo', {
-      // timeStampは一意なので、keyPathとして使う
-      keyPath: 'timeStamp'
+  },
+  addTodo: function(text){
+    const db = module.db;
+    // DBからObjectStoreへのトランザクションを生成する
+    // この段階でtodoというObjectStoreをつくってないとエラーを吐く
+    // アクティブなobjectの生成
+    const transaction = db.transaction('todo', 'readwrite');
+    const todoObjectStore = transaction.objectStore('todo');
+    // putするリクエストを生成
+    todoObjectStore.put({
+      text: text,
+      timeStamp: Date.now()
     });
-    console.log('store', store);
-  };
-
-  // DBを開くリクエスト
-  // ここが正常に上手くいった場合は、保持しておく
-  request.onsuccess = (event) => {
-    module.db = event.target.result;
-    // 初期表示時に、保存されている物を全て取っておく
-    module.getAll(module.renderer);
-  }
-};
-
-// TODOを追加する関数
-module.addTodo = (text) => {
-  const db = module.db;
-  // DBからObjectStoreへのトランザクションを生成する
-  // この段階でtodoというObjectStoreをつくってないとエラーを吐く
-  // アクティブなobjectの生成
-  const transaction = db.transaction('todo', 'readwrite');
-  const todoObjectStore = transaction.objectStore('todo');
-  // putするリクエストを生成
-  todoObjectStore.put({
-    text: text,
-    timeStamp: Date.now()
-  });
-  transaction.oncomplete = () => {
-    // リクエスト成功時は、リストに反映
-    module.getAll(module.renderer);
-  };
-  transaction.onerror = (error) => {
-    alert(error);
-  };
-};
-
-// TODOを全て取得する関数
-module.getAll = function (renderer) {
-  if (renderer) {
-    document.querySelector('.todoList').innerHTML = '';
-  }
-  const db = module.db;
-  const transaction = db.transaction('todo', 'readwrite');
-  const store = transaction.objectStore('todo');
-
-  // keyPathに対して検索をかける範囲を取得
-  const range = IDBKeyRange.lowerBound(0);
-  const cursorRequest = store.openCursor(range);
-
-  // カーソルリクエストの成功
-  cursorRequest.onsuccess = (event) => {
-    const result = event.target.result;
-    // 走査すべきObjectがこれ以上ない場合
-    if (!!result == false) {
-      return;
-    }
+    transaction.oncomplete = () => {
+      // リクエスト成功時は、リストに反映
+      module.getAllTodo(module.renderer);
+    };
+    transaction.onerror = (error) => {
+      alert(error);
+    };
+  },
+  getAllTodo: function(renderer){
     if (renderer) {
-      renderer(result.value);
+      document.querySelector('.todoList').innerHTML = '';
     }
-    // マッチした場合は処理を続行
-    result.continue();
-  };
+    const db = module.db;
+    const transaction = db.transaction('todo', 'readwrite');
+    const store = transaction.objectStore('todo');
 
-  // カーソルリクエストの失敗
-  cursorRequest.onerror = (error) => {
-    alert(error);
-  };
+    // keyPathに対して検索をかける範囲を取得
+    const range = IDBKeyRange.lowerBound(0);
+    const cursorRequest = store.openCursor(range);
+
+    // カーソルリクエストの成功
+    cursorRequest.onsuccess = (event) => {
+      const result = event.target.result;
+      // 走査すべきObjectがこれ以上ない場合
+      if (!!result == false) {
+        return;
+      }
+      if (renderer) {
+        renderer(result.value);
+      }
+      // マッチした場合は処理を続行
+      result.continue();
+    };
+
+    // カーソルリクエストの失敗
+    cursorRequest.onerror = (error) => {
+      alert(error);
+    };
+  }
 };
 
 (function(){
